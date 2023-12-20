9e770b4dbcf4f01fbae8f4dc39b5b2699bf98344
Deepanshi Sharma <deepanshisharma2510@gmail.com>
2023-12-20 21:02:12 +0530

    added source  code  link

--- src/App.js
+++ src/App.js
@@ -5,11 +5,20 @@ import StudentDetails from "./pages/StudentDetails";
 import Teachers from "./features/teachers/Teachers";
 import TeacherDetails from "./pages/TeacherDetails";
 import SchoolStatistics from "./pages/SchoolStatistics";
+import { NavLink } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <h1>School Management App</h1>
+      <h1>
+        School Management App{" "}
+        <span style={{ fontSize: "small" }}>
+          {" "}
+          <NavLink to="https://github.com/deepanshimarsha/school-management">
+            (Source Code)
+          </NavLink>
+        </span>
+      </h1>
 
       <Routes>
         <Route path="/" element={<Students />} />
